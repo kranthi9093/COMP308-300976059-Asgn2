ee9a535267817a82cad6250f5eb0519a80ada9f1
kranthikumar <kranthiveer7@gmail.com>
2019-04-06 22:57:54 -0400

    first commit-previous package was failed in heroku due to version changes.so copied that package and try in new repository

---
+++ server/config/db.js
@@ -0,0 +1,11 @@
+/* Project Name: COMP308 Assignment2
+   Name: Kranthi kumar G J
+   Student id: 300976059
+   Date: 05-APR-2019 */
+
+module.exports = {
+    //"URI" : "mongodb://kranthi93:abc123@ds064278.mlab.com:64278/comp308-lesson4a",
+    "URI" : "mongodb://kranthi93:abc123@ds054999.mlab.com:54999/comp308-lesson4",
+    //"URI": "mongodb://localhost/test",
+    "secret": "SomeSecret"
+}
